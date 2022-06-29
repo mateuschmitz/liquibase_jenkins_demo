d22795d571b7b0fc1fe6d6958bf8e63a78c63038
Mateus Schmitz <mateus.schmitz@solfacil.com.br>
2022-06-29 11:00:48 -0300

    Adding liquibase config and first changeset

---
+++ jenkins/rollbackCount.groovy
@@ -0,0 +1,19 @@
+pipeline {
+    agent any
+    stages {
+        stage('Configure Credentials') {
+            steps {
+                sh '''
+                git config --global user.email "jenkins@solfacil.com.br" && git config --global user.name "Jenkins" && echo "driver: org.postgresql.Driver\nurl: jdbc:postgresql://postgres_test:5432/solfacil\nusername: solfacil\npassword: solfacil\nchangeLogFile: ./db.changelog.xml\nliquibase.hub.mode=off" > liquibase.properties
+                '''
+            }
+        }
+        stage('Rollback Database') {
+            steps {
+                sh '''
+                    /opt/liquibase/liquibase rollbackCount ${ROLLBACK_COUNT}
+                '''
+            }
+        }
+    }
+}
